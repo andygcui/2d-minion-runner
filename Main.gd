70269b34814f73032cd6d97fecc4991c4d816feb
andygcui <andygcui@gmail.com>
2025-11-12 12:40:20 -0500

    Create Main.gd

---
+++ Main.gd
@@ -0,0 +1,61 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$DeathSound.play()
+	$Music.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate() # Create new instance of Mob scene
+	
+	# Create random location on Path2D
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	# Set mob direction perpendicular to the path direction
+	var direction = mob_spawn_location.rotation + PI/ 2
+	
+	# Set mob position to random location
+	mob.position = mob_spawn_location.position
+	
+	# Random direction
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# Random velocity
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# Spawn mob
+	add_child(mob)
+
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
